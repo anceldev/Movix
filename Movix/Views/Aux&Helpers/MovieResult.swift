54bcc1623526ba94fe906fe0858d2cb26273f7e1
Ancel Dev account <anceldev@Ancels-MacBook-Pro.local>
2023-11-02 01:16:15 +0100

    MovieSerice

---
+++ Movix/Views/Aux&Helpers/MovieResult.swift
@@ -0,0 +1,18 @@
+//
+//  MovieResult.swift
+//  Movix
+//
+//  Created by Ancel Dev account on 1/11/23.
+//
+
+import SwiftUI
+
+struct MovieResult: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovieResult()
+}
